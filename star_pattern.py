3894d2eab55aa943e3dbf4b9070cc85f7241b2f9
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-12 03:32:11 +0800

    Update star_pattern.py

--- star_pattern.py
+++ star_pattern.py
@@ -16,6 +16,12 @@ def triangle_right_angled_inverted(star):
         print("\r")
 
 
+def equilateral_triangle(star):
+    for rows in range(1, star + 1):
+        print(" " * (star - rows), end="")
+        print("* " * rows)
+
+
 print()
 triangle_right_angled(5)
 
@@ -25,3 +31,5 @@ triangle_right_angled_inverted(5)
 print()
 triangle_right_angled(3)
 triangle_right_angled_inverted(3)
+
+equilateral_triangle(5)
