bc2e2f23db690d0cbd7ad3753c97e3556e95d38b
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-17 11:39:17 +0800

    Update test.py

--- Sorting Timestamps/test.py
+++ Sorting Timestamps/test.py
@@ -5,6 +5,11 @@ from sorting_timestamps import extract_distinct_dates, find_consecutive_sequence
 
 class TestLoginSequences(unittest.TestCase):
 
+    def setUp(self):
+        self.distinct_dates = [
+            datetime(2021, 3, 13), datetime(2021, 3, 16), datetime(2021, 3, 17), datetime(2021, 3, 18)
+        ]
+
     def test_extract_distinct_dates(self):
         timestamps = [
             '2021-03-13 15:13:05', '2021-03-13 23:13:05', '2021-03-16 15:13:05',
@@ -12,17 +17,13 @@ class TestLoginSequences(unittest.TestCase):
             '2021-03-17 23:13:05', '2021-03-18 07:13:05', '2021-03-18 15:13:05'
         ]
 
-        expected_dates = [datetime(2021, 3, 13), datetime(2021, 3, 16), datetime(2021, 3, 17), datetime(2021, 3, 18)]
-
-        self.assertEqual(extract_distinct_dates(timestamps), expected_dates)
+        self.assertEqual(extract_distinct_dates(timestamps), self.distinct_dates)
 
     def test_find_consecutive_sequences(self):
-        dates = [datetime(2021, 3, 13), datetime(2021, 3, 16), datetime(2021, 3, 17), datetime(2021, 3, 18)]
-
         expected_sequences = [(datetime(2021, 3, 16), datetime(2021, 3, 18), 3),
                               (datetime(2021, 3, 13), datetime(2021, 3, 13), 1)]
 
-        self.assertEqual(find_consecutive_sequences(dates), expected_sequences)
+        self.assertEqual(find_consecutive_sequences(self.distinct_dates), expected_sequences)
 
 
 if __name__ == '__main__':
