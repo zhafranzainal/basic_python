fc7f3d2d7543a4c0848fc0c180f83ab4bffd33d9
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-15 15:21:42 +0800

    Update course_parser.py

--- Course Catalogue Parser/course_parser.py
+++ Course Catalogue Parser/course_parser.py
@@ -8,11 +8,11 @@ with pdfplumber.open("COURSE_CATALOG_IJA.pdf") as pdf:
         text = page.extract_text()
         extracted_text += text
 
-extracted_text.replace("\n", " ").replace("03-OCT-23 11:09 AM", "")
+extracted_text = extracted_text.replace("\n", " ").replace("03-OCT-23 11:09 AM", "")
 
-pattern = r"Course Code : ([A-Z]{3}\d{4})+((?:(?!Course Code :).)*)Course Name : (.+?)\n"
-matches = re.findall(pattern, extracted_text, re.DOTALL)
+pattern = r"Course Code : ([A-Z]{3}\d{4})[\s\S]+?Course Name : (.+?)\d"
+matches = re.findall(pattern, extracted_text)
 
 for match in matches:
-    course_code, extra_text, course_name = match
-    print(f"Course Code: {course_code}\nCourse Name: {course_name}\n")
+    print(f"Course Code: {match[0]}")
+    print(f"Course Name: {match[1]}\n")
