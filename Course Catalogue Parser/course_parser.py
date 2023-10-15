ab591b23db9eadb38870b5a273fbcacbad7c742d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-15 16:53:33 +0800

    Remove unnecessary data

--- Course Catalogue Parser/course_parser.py
+++ Course Catalogue Parser/course_parser.py
@@ -8,7 +8,11 @@ with pdfplumber.open("COURSE_CATALOG_IJA.pdf") as pdf:
         text = page.extract_text()
         extracted_text += text
 
-extracted_text = extracted_text.replace("\n", " ").replace("03-OCT-23 11:09 AM", "")
+extracted_text = extracted_text \
+    .replace("\n", " ") \
+    .replace("03-OCT-23 11:09 AM", "") \
+    .replace("Pre-Requisite : NO ", "") \
+    .replace("Remarks : NO ", "")
 
 pattern = r"Course Code : ([A-Z]{3}\d{4})[\s\S]+?Course Name : (.+?)\d"
 matches = re.findall(pattern, extracted_text)
