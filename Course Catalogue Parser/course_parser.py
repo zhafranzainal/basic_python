80ed47aa9a43e9c90577afc01299a9b7de87f4f5
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-15 17:57:21 +0800

    Display each table under its corresponding course code

--- Course Catalogue Parser/course_parser.py
+++ Course Catalogue Parser/course_parser.py
@@ -3,12 +3,6 @@ import re
 import camelot
 from IPython.display import display
 
-tables = camelot.read_pdf('https://or.ump.edu.my/or/CourseCatalog/COURSE_CATALOG_IJA.pdf', pages='345-384')
-
-for i in range(len(tables)):
-    tables[i].df = tables[i].df.replace('\\n', ' ', regex=True)
-    display(tables[i].df)
-
 with pdfplumber.open("COURSE_CATALOG_IJA.pdf") as pdf:
     extracted_text = ''
 
@@ -24,9 +18,14 @@ extracted_text = extracted_text \
 
 pattern = r"Course Code : ([A-Z]{3}\d{4})[\s\S]+?Course Name : (.+?)\d"
 matches = re.findall(pattern, extracted_text)
-counter = 1
 
-for match in matches:
-    print(f"{counter}. Course Code: {match[0]}")
+tables = camelot.read_pdf('https://or.ump.edu.my/or/CourseCatalog/COURSE_CATALOG_IJA.pdf', pages='345-384')
+
+for i, match in enumerate(matches):
+    print(f"{i + 1}. Course Code: {match[0]}")
     print(f"Course Name: {match[1]}\n")
-    counter += 1
+
+    if i < len(tables):
+        table = tables[i]
+        table.df = table.df.applymap(lambda x: x.replace('\n', ' '))
+        display(table.df)
