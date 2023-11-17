ccc805438e3b33d87236ab6e3a8b735b34b288c0
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-11-17 17:21:13 +0800

    Clean table 27

--- Course Catalogue Parser/course_parser.py
+++ Course Catalogue Parser/course_parser.py
@@ -517,6 +517,24 @@ modes = tables[25].df.iloc[16, 4].split()[:16]
 for i in range(13, 21):
     tables[25].df.iloc[i, 4] = modes[2 * (i - 13)] + modes[2 * (i - 13) + 1]
 
+# TABLE 27 SEM I
+# Split "Sec Day Time Loc Mode Cap" into its respective column
+tables[26].df.iloc[1, :6] = tables[26].df.iloc[1, 0].split()[:6]
+
+# Split column "mode" into its respective row
+modes = tables[26].df.iloc[4, 4].split()[:10]
+for i in range(2, 7):
+    tables[25].df.iloc[i, 4] = modes[2 * (i - 2)] + modes[2 * (i - 2) + 1]
+
+# TABLE 27 SEM II
+# Split "Sec Day Time Loc Mode Cap" into its respective column
+tables[26].df.iloc[8, :6] = tables[26].df.iloc[8, 0].split()[:6]
+
+# Split column "mode" into its respective row
+modes = tables[26].df.iloc[10, 4].split()[:6]
+for i in range(9, 12):
+    tables[26].df.iloc[i, 4] = modes[2 * (i - 9)] + modes[2 * (i - 9) + 1]
+
 # Display each table under its corresponding course code
 for i, match in enumerate(matches):
     print(f"{i + 1}. Course Code: {match[1]}")
