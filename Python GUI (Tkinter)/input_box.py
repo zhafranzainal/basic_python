65e431d35d6e41cc4d1c1bd82c6777ee1f5bdd05
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-07-19 06:20:14 +0800

    Add label in input box

--- Python GUI (Tkinter)/input_box.py
+++ Python GUI (Tkinter)/input_box.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Entry
+from tkinter import Tk, Entry, Label
 
 # Create the main window
 root = Tk()
@@ -6,6 +6,10 @@ root.title("Tkinter Example")
 
 # Set the window size (width x height)
 root.geometry("400x100")
+
+# Create a label
+label = Label(root, text="Enter Your Name:")
+label.pack()
 
 # Create an input box (Entry)
 entry = Entry(root)
